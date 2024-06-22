ba4739e60883dffbdfb2b90a12976f38d59ca02d
Chad Linthicum <portableanswers@gmail.com>
2024-06-22 02:58:24 -0400

    -WebGL build adjustments

--- Assets/Scripts/Managers/OurSceneManager.cs
+++ Assets/Scripts/Managers/OurSceneManager.cs
@@ -1,16 +1,12 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEditor.SceneManagement;
 using UnityEngine.SceneManagement;
-using UnityEditor.SearchService;
 
 public class OurSceneManager : MonoBehaviour
 {
-
-    
-
     public int sceneIndex;
+
     public void SceneSwitcher()
     {
         SceneManager.LoadScene(sceneIndex);
@@ -23,31 +19,4 @@ public class OurSceneManager : MonoBehaviour
             SceneManager.LoadScene(0);
         }
     }
-
-
-    //public enum Scenes { Beach, Forest, Garage, Snow, Start }
-
-    //public void SceneSwitch(Scenes sceneName)
-    //{
-    //    SceneManager.LoadScene(GetSceneName(sceneName));
-    //}
-
-    //public string GetSceneName(Scenes sceneName)
-    //{
-    //    switch (sceneName)
-    //    {
-    //        case Scenes.Beach:
-    //            return "Beach";
-    //        case Scenes.Forest:
-    //            return "Forest";
-    //        case Scenes.Garage:
-    //            return "Garage";
-    //        case Scenes.Snow:
-    //            return "Snow";
-    //        case Scenes.Start:
-    //            return "Start";
-    //        default:
-    //            return "";
-    //    }
-    //}
 }
